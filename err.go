f8ca4260c2e3b040933b04655a4311ccfcef5b50
Nifemi <soneyenifemi@gmail.com>
2026-09-04 11:09:02 +0100

    Finished Go error chapter

--- err.go
+++ err.go
@@ -1,35 +1,24 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 )
 
-func sendSMSToCouple(msgToCustomer, msgToSpouse string) (int, error) {
-	costForCustomer , err := sendSMS(msgToCustomer)
-
-	if err != nil {
-		return 0 , err
-	}
-	costForSpouse , err := sendSMS(msgToSpouse)
-
-	if err != nil {
-		return 0 , err
-	}
-	return costForCustomer + costForSpouse , nil
+type divideError struct {
+	dividend float64
 }
 
-// don't edit below this line
+// ?
 
-func sendSMS(message string) (int, error) {
-	const maxTextLen = 25
-	const costPerChar = 2
-	if len(message) > maxTextLen {
-		return 0, fmt.Errorf("can't send texts over %v characters", maxTextLen)
+func divide(dividend, divisor float64) (float64, error) {
+	if divisor == 0 {
+		return 0.0 , errors.New("No dividing by 0")
 	}
-	return costPerChar * len(message), nil
+	return dividend / divisor, nil
 }
 
 func main() {
-	cost, err := sendSMSToCouple("hello", "hey there")
-	fmt.Println(cost, err)
+	value , message := divide(16 , 0)
+	fmt.Println(value , message)
 }
