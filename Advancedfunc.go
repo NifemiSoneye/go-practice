bde976063b2fdfd7007b402c39fcc1856e56305f
Nifemi <soneyenifemi@gmail.com>
2026-09-08 14:21:33 +0100

    Anonymous func chapter

--- Advancedfunc.go
+++ Advancedfunc.go
@@ -2,49 +2,39 @@ package main
 
 import "fmt"
 
-func adder() func(int) int {
-	sum := 0 
-	return func (x int) int {
-		sum += x
-		return sum
+func printReports(messages []string) {
+	for _ ,message := range messages {
+		printCostReport(func (msg string)int {
+			return len(msg) * 2
+		} , message)
 	}
 }
 
 // don't touch below this line
 
-type emailBill struct {
-	costInPennies int
-}
-
-func test(bills []emailBill) {
+func test(messages []string) {
 	defer fmt.Println("====================================")
-	countAdder, costAdder := adder(), adder()
-	for _, bill := range bills {
-		fmt.Printf("You've sent %d emails and it has cost you %d cents\n", countAdder(1), costAdder(bill.costInPennies))
-	}
+	printReports(messages)
 }
 
 func main() {
-	test([]emailBill{
-		{45},
-		{32},
-		{43},
-		{12},
-		{34},
-		{54},
+	test([]string{
+		"Here's Johnny!",
+		"Go ahead, make my day",
+		"You had me at hello",
+		"There's no place like home",
 	})
 
-	test([]emailBill{
-		{12},
-		{12},
-		{976},
-		{12},
-		{543},
+	test([]string{
+		"Hello, my name is Inigo Montoya. You killed my father. Prepare to die.",
+		"May the Force be with you.",
+		"Show me the money!",
+		"Go ahead, make my day.",
 	})
+}
 
-	test([]emailBill{
-		{743},
-		{13},
-		{8},
-	})
+func printCostReport(costCalculator func(string) int, message string) {
+	cost := costCalculator(message)
+	fmt.Printf(`Message: "%s" Cost: %v cents`, message, cost)
+	fmt.Println()
 }
