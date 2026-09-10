8a20d42ac3e7a350aba9d128aa31e16b4ed23f61
Nifemi <soneyenifemi@gmail.com>
2026-09-10 14:51:41 +0100

    Buffered channels

--- channels.go
+++ channels.go
@@ -1,100 +1,34 @@
 package main
 
-import (
-	"fmt"
-	"time"
-)
+import "fmt"
 
-func filterOldEmails(emails []email) {
-	isOldChan := make(chan bool)
-
-
-	go func () {
-		for _, e := range emails {
-		if e.date.Before(time.Date(2020, 0, 0, 0, 0, 0, 0, time.UTC)) {
-			isOldChan <- true
-			continue
-		}
-		isOldChan <- false
+func addEmailsToQueue(emails []string) chan string {
+	emailsToSend := make(chan string , len(emails))
+	for _, email := range emails {
+		emailsToSend <- email
 	}
-	} ()
-	
-
-	isOld := <-isOldChan
-	fmt.Println("email 1 is old:", isOld)
-	isOld = <-isOldChan
-	fmt.Println("email 2 is old:", isOld)
-	isOld = <-isOldChan
-	fmt.Println("email 3 is old:", isOld)
+	return emailsToSend
 }
 
-// TEST SUITE -- Don't touch below this line
+// TEST SUITE - Don't Touch Below This Line
 
-type email struct {
-	body string
-	date time.Time
+func sendEmails(batchSize int, ch chan string) {
+	for i := 0; i < batchSize; i++ {
+		email := <-ch
+		fmt.Println("Sending email:", email)
+	}
 }
 
-func test(emails []email) {
-	filterOldEmails(emails)
+func test(emails ...string) {
+	fmt.Printf("Adding %v emails to queue...\n", len(emails))
+	ch := addEmailsToQueue(emails)
+	fmt.Println("Sending emails...")
+	sendEmails(len(emails), ch)
 	fmt.Println("==========================================")
 }
 
 func main() {
-	test([]email{
-		{
-			body: "Are you going to make it?",
-			date: time.Date(2019, 0, 0, 0, 0, 0, 0, time.UTC),
-		},
-		{
-			body: "I need a break",
-			date: time.Date(2021, 0, 0, 0, 0, 0, 0, time.UTC),
-		},
-		{
-			body: "What were you thinking?",
-			date: time.Date(2022, 0, 0, 0, 0, 0, 0, time.UTC),
-		},
-	})
-	test([]email{
-		{
-			body: "Yo are you okay?",
-			date: time.Date(2018, 0, 0, 0, 0, 0, 0, time.UTC),
-		},
-		{
-			body: "Have you heard of that website Boot.dev?",
-			date: time.Date(2017, 0, 0, 0, 0, 0, 0, time.UTC),
-		},
-		{
-			body: "It's awesome honestly.",
-			date: time.Date(2016, 0, 0, 0, 0, 0, 0, time.UTC),
-		},
-	})
-	test([]email{
-		{
-			body: "Today is the day!",
-			date: time.Date(2019, 0, 0, 0, 0, 0, 0, time.UTC),
-		},
-		{
-			body: "What do you want for lunch?",
-			date: time.Date(2021, 0, 0, 0, 0, 0, 0, time.UTC),
-		},
-		{
-			body: "Why are you the way that you are?",
-			date: time.Date(2022, 0, 0, 0, 0, 0, 0, time.UTC),
-		},
-	})
-	test([]email{
-		{
-			body: "Did we do it?",
-			date: time.Date(2019, 0, 0, 0, 0, 0, 0, time.UTC),
-		},
-		{
-			body: "Letsa Go!",
-			date: time.Date(2021, 0, 0, 0, 0, 0, 0, time.UTC),
-		},
-		{
-			body: "Okay...?",
-			date: time.Date(2022, 0, 0, 0, 0, 0, 0, time.UTC),
-		},
-	})
+	test("Hello John, tell Kathy I said hi", "Whazzup bruther")
+	test("I find that hard to believe.", "When? I don't know if I can", "What time are you thinking?")
+	test("She says hi!", "Yeah its tomorrow. So we're good.", "Cool see you then!", "Bye!")
 }
